1f14522cdc2a9d18dad04b1f14d5332a67c1c1aa
Richard Brenick <RichardBrenick@gmail.com>
2019-06-15 22:05:14 +0200

    fixed get_latest_version

--- _install_/get_latest_version.ps1
+++ _install_/get_latest_version.ps1
@@ -30,7 +30,7 @@ function SafeRemove
 # Lotsa variables
 $USER_DOWNLOADS_FOLDER = "$env:userprofile\Downloads"
 
-$TOOL_GIT_REPO = "https://github.com/rBrenick/maya-tool-template/archive/master.zip" # modify these after uploading the tool
+$TOOL_GIT_REPO = "https://github.com/rBrenick/script-tree/archive/master.zip" # modify these after uploading the tool
 $UPDATE_PATHS = @("docs", "src", "README.md")
 
 $CURRENT_TOOL_FOLDER = (Get-Item -Path ".").FullName
